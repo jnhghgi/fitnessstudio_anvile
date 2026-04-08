bb54295d543da28453bf2bb67116335941d9bb29
luis.birnbaumer@student.htl-rankweil.at <luis.birnbaumer@student.htl-rankweil.at>
2026-04-08 09:36:23 +0000

    Created Backend, Frontend, Frontend.Anmeldung, Frontend.Anmeldung.RowTemplate2 and 2 more; Edited settings; Deleted Form1

---
+++ server_code/Backend.py
@@ -0,0 +1,77 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import sqlite3
+
+
+@anvil.server.callable
+def get_buchungs_liste():
+  conn = sqlite3.connect('Birnbaumer_Luis_fitnessstudio.db')
+  cursor = conn.cursor()
+
+  query = '''
+SELECT 
+    k.bezeichnung AS bezeichnung, 
+    k.termin AS termin,
+    t.name AS name,
+    k.maxTeilnehmer AS maxTeilnehmer,
+    COUNT(b.mitgliedID) AS aktuelleTeilnehmer
+FROM kurse k
+JOIN Trainer t ON k.trainerID = t.trainerID
+LEFT JOIN Bucht b ON k.kursID = b.kursID
+GROUP BY k.kursID
+    '''
+  cursor.execute(query)
+  rows = cursor.fetchall()
+
+  ergebnis = []
+  for row in rows:
+    belegungs_string = f"{row['aktuelle_anzahl']} / {row['maxTeilnehmer']}"
+    ergebnis.append({
+      'kurs': row['bezeichnung'],
+      'name': row['name'],
+      'termin':row['termin'],
+      'belegung': belegungs_string,
+    })
+  return ergebnis
+
+
+
+
+
+@anvil.server.callable
+def get_mitglieder(kurs):
+  conn = sqlite3.connect('Birnbaumer_Luis_fitnessstudio.db')
+  cursor = conn.cursor()
+
+  query = '''
+SELECT 
+    k.bezeichnung AS bezeichnung, 
+    k.termin AS termin,
+    t.name AS name,
+    k.maxTeilnehmer AS maxTeilnehmer,
+    COUNT(b.mitgliedID) AS aktuelleTeilnehmer
+FROM kurse k
+JOIN Trainer t ON k.trainerID = t.trainerID
+LEFT JOIN Bucht b ON k.kursID = b.kursID
+Where 
+    '''
+  cursor.execute(query)
+  rows = cursor.fetchall()
+
+  ergebnis = []
+  for row in rows:
+    belegungs_string = f"{row['aktuelle_anzahl']} / {row['maxTeilnehmer']}"
+    ergebnis.append({
+      'kurs': row['bezeichnung'],
+      'name': row['name'],
+      'termin':row['termin'],
+      'belegung': belegungs_string,
+    })
+  return ergebnis
+
+
+
